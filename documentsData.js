45a9db37e76dd0ded2b00ac1c854e84196aec85a
letuannghia2k4 <117922660+letuannghia2k4@users.noreply.github.com>
2025-10-28 04:31:41 +0700

    update mới

---
+++ documentsData.js
@@ -0,0 +1,41 @@
+const documentsList = [
+    {
+        name: "Cấu Trúc Dữ Liệu",
+        tag: "Tài liệu miễn phí",
+        downloads: "101",
+        image: "placeholder-caudl.jpg", // Thay bằng đường dẫn ảnh thật
+        files: [
+            // ĐƯỜNG DẪN MỚI: Trỏ vào thư mục "documents/"
+            { name: "Chương 1: Giới thiệu (PDF)", url: "TL_SQL/Slide_Ch09_SQL Co ban.pdf" },           
+        ], 
+    },
+    {
+        name: "Tài Liệu Học Lập Trình C",
+        tag: "Tài liệu miễn phí",
+        downloads: "110",
+        image: "placeholder-laptrinhc.jpg",
+        link: "#",
+    },
+    {
+        name: "Tài Liệu Học Lập Trình Hướng Đối Tượng",
+        tag: "Tài liệu miễn phí",
+        downloads: "94",
+        image: "placeholder-oop.jpg",
+        link: "#",
+    },
+    // Thêm các mục khác (Xây dựng, SEO Google) tại đây
+    {
+        name: "Tự Xây Dựng Thành Công",
+        tag: "Xây Dựng",
+        downloads: "921",
+        image: "placeholder-xaydung.jpg",
+        link: "#",
+    },
+    {
+        name: "Tài Liệu SEO Google",
+        tag: "SEO",
+        downloads: "124",
+        image: "placeholder-seo.jpg",
+        link: "#",
+    },
+];
---
+++ softwareData.js
@@ -0,0 +1,44 @@
+const softwareList = [
+    {
+        name: "Lab View 2017",
+        tag: "LabVIEW",
+        downloads: "296",
+        image: "images/placeholder-labview.jpg", // Thay bằng đường dẫn ảnh thật
+        link: "#", 
+    },
+    {
+        name: "Altium Designer",
+        tag: "Altium Designer",
+        downloads: "710",
+        image: "images/placeholder-altium.jpg",
+        link: "#",
+    },
+    {
+        name: "Proteus Bản Full",
+        tag: "Proteus Full",
+        downloads: "20.247",
+        image: "images/placeholder-proteus.jpg",
+        link: "#",
+    },
+    {
+        name: "Microsoft Office 365",
+        tag: "Office 365",
+        downloads: "969",
+        image: "images/placeholder-office365.jpg",
+        link: "#",
+    },
+    {
+        name: "MiniTool Partition Wizard",
+        tag: "Partition Wizard",
+        downloads: "1.501",
+        image: "images/placeholder-partition.jpg",
+        link: "#",
+    },
+    {
+        name: "Fritzing Electronic Win 10",
+        tag: "Fritzing",
+        downloads: "309",
+        image: "images/placeholder-fritzing.jpg",
+        link: "#",
+    },
+];
